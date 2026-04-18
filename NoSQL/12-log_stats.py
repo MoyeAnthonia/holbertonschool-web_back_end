4693e70f9d572c93d996919373074ad1ea394add
MoyeAnthonia <moye.anthonia@gmail.com>
2026-04-19 00:25:37 +1000

    final task done

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""MongoDb IN ptyhon"""
+
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(f"{collection.count_documents({})} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        print(f"\tmethod {m}: {collection.count_documents({'method': m})}")
+
+    filter_status = {'method': 'GET', 'path': '/status'}
+    print(f"{collection.count_documents(filter_status)} status check")
