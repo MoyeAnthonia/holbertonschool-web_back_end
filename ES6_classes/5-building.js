6f8e33600b9123476b92c10f4bd975705b61f017
MoyeAnthonia <moye.anthonia@gmail.com>
2026-04-24 10:56:48 +1000

    fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,34 +1,30 @@
 export default class Building {
   /**
-   * @param {Number} sqft - sqft
+   * @param {Number} sqft - The square footage of the building.
    */
   constructor(sqft) {
-    // 1. Abstract Class Simulation: Prevent direct instantiation
-    if (this.constructor === Building) {
-      throw new Error(
-        "Building is an abstract class and cannot be instantiated directly",
-      );
+    // Abstract class check
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== "function") {
+        throw new Error(
+          "Class extending Building must override evacuationWarningMessage",
+        );
+      }
     }
 
-    // 2. Implementation Check: Ensure child classes have the required method
-    if (this.evacuationWarningMessage === undefined) {
-      throw new Error(
-        "Class extending Building must override evacuationWarningMessage",
-      );
-    }
     this.sqft = sqft;
   }
 
+  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Setter
+  // Setter for sqft
   set sqft(value) {
     if (typeof value !== "number") {
-      throw new TypeError("Square footage must be a number");
-    } else {
-      this._sqft = value;
+      throw new TypeError("sqft must be a number");
     }
+    this._sqft = value;
   }
 }
