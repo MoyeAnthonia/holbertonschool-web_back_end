ce3c9de74f512fe54b2e7baee0dc1459aea5473a
MoyeAnthonia <moye.anthonia@gmail.com>
2026-05-11 14:55:01 +1000

    tasks 0-1 done

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI(success) {
+  if(success === true) {
+    return Promise.resolve({
+      status: 200,
+      body: "Success",
+    });
+  } else {
+    return Promise.reject(new Error("The fake API is not working currently"));
+  }
+}
