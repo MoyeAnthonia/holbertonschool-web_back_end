2ed914a74f9efcd9aa9706dd2cafb4eac680d4b8
MoyeAnthonia <moye.anthonia@gmail.com>
2026-05-13 23:06:23 +1000

    fix

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,9 +1,15 @@
-/**
- * Prints a message to the standard output.
- * @param {String} message - The message to display.
- */
+// /**
+//  * Prints a message to the standard output.
+//  * @param {String} message - The message to display.
+//  */
+// function displayMessage(message) {
+//   process.stdout.write(`${message}\n`);
+// }
+
+// module.exports = displayMessage;
+
 function displayMessage(message) {
-  process.stdout.write(`${message}\n`);
+  console.log(message);
 }
 
 module.exports = displayMessage;
