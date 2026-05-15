bfa7dd1a5827b8e815ad04753e5c543ba8e3ea94
MoyeAnthonia <moye.anthonia@gmail.com>
2026-05-15 22:09:33 +1000

    tasks done

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+function countStudents(fileName) {
+  const filePath = path.join(__dirname, fileName);
+
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    const validStudents = data
+      .split("\n")
+      .slice(1)
+      .filter((line) => line.trim() !== "");
+
+    const fields = {};
+
+    for (const row of validStudents) {
+      const [, , , field] = row.split(",");
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(row.split(",")[0]);
+    }
+    console.log(`Number of students: ${validStudents.length}`);
+    for (const structure of Object.keys(fields)) {
+      console.log(
+        `Number of students in ${structure}: ${fields[structure].length}. List: ${fields[structure].join(", ")}`,
+      );
+    }
+  } catch (err) {
+    throw new Error("Cannot load the database");
+  }
+}
+
+module.exports = countStudents;
